ceedc59d0b0ce46a87c1a98ecabda62831d127d0
CarolPatricio <carolpatricio28@hotmail.com>
2024-10-14 17:07:38 -0300

    Ajustes no estilo

--- futebol_historico/blog/static/blog/js/scripts.js
+++ futebol_historico/blog/static/blog/js/scripts.js
@@ -1,15 +1,29 @@
 // blog/static/blog/js/scripts.js
 
-let slideIndex = 0;
-showSlides();
+// let slideIndex = 0;
+// showSlides();
 
-function showSlides() {
-    let slides = document.querySelectorAll('.slides img');
-    slides.forEach((slide, index) => {
-        slide.style.display = (index === slideIndex) ? 'block' : 'none';
+// function showSlides() {
+//     let slides = document.querySelectorAll('.slides img');
+//     slides.forEach((slide, index) => {
+//         slide.style.display = (index === slideIndex) ? 'block' : 'none';
+//     });
+//     slideIndex = (slideIndex + 1) % slides.length;
+//     setTimeout(showSlides, 3000); // Troca de slide a cada 3 segundos
+// }
+
+$(document).ready(function() {
+    $(".mobile-menu-button").on("click", function() {
+        // Adiciona ou remove a classe "ativo" do header
+        $(".header-principal").toggleClass('ativo');
+        
+        // Alterna a exibição do ícone hamburguer e do ícone de fechamento
+        $(this).find('.hamburger-icon').toggle();
+        $(this).find('.close-icon').toggle();
+        
+        console.log("oi");
     });
-    slideIndex = (slideIndex + 1) % slides.length;
-    setTimeout(showSlides, 3000); // Troca de slide a cada 3 segundos
-}
+});
+
 
 
